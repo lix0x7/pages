cf58dba693ed096e3695e0ce44954cc1759d545d
luxxli <luxxli@tencent.com>
2022-10-11 15:13:00 +0800

    fix

--- server/main.go
+++ server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"os/exec"
 )
 
 func main() {
@@ -18,6 +20,17 @@ func main() {
 	r.GET("/index", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"title": "lix7",
+		})
+	})
+	r.GET("/version", func(c *gin.Context) {
+		cmd := exec.Command("git", "rev-parse", "--short", "HEAD")
+		version, err := cmd.Output()
+		if err != nil {
+			log.Printf("version: %s, err: %s, path: %s", version, err, cmd.Path)
+			version = []byte("error")
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"version": string(version),
 		})
 	})
 	r.Run("0.0.0.0:80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
